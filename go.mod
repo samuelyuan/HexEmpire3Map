e35e5d0b815f518cda04688d55a7c101dfab7569
Samuel Yuan <samuelyuan@users.noreply.github.com>
2025-12-15 02:47:27 +0000

    Improve code organization

--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module github.com/samuelyuan/HexEmpire3Map
 
-go 1.22.0
+go 1.24.0
 
 require (
 	github.com/fogleman/gg v1.3.0
-	golang.org/x/text v0.20.0
+	golang.org/x/text v0.32.0
 )
 
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	golang.org/x/image v0.22.0 // indirect
+	golang.org/x/image v0.34.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"math"
 	"os"
@@ -24,10 +23,22 @@ type MapData struct {
 	Depth    int
 }
 
+const (
+	HexRadius = 10.0
+)
+
 var (
 	NeighborOdd  = [6][2]int{{-1, 0}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}}
 	NeighborEven = [6][2]int{{-1, 0}, {-1, -1}, {0, -1}, {1, 0}, {0, 1}, {-1, 1}}
-	PartyColors  = [6][3]int{{0, 76, 229}, {178, 0, 204}, {255, 8, 8}, {0, 153, 0}, {204, 127, 0}, {0, 127, 115}}
+	// PartyColors represents the colors for each faction
+	PartyColors = [6][3]int{
+		{0, 76, 229},   // Party 0: Bluegaria (Blue)
+		{178, 0, 204},  // Party 1: Violetnam (Purple)
+		{255, 8, 8},    // Party 2: Redosia (Red)
+		{0, 153, 0},    // Party 3: Greenland (Green)
+		{204, 127, 0},  // Party 4: Amberica (Amber)
+		{0, 127, 115},  // Party 5: Turquoistan (Turquoise)
+	}
 )
 
 func readData(filename string) (*MapData, error) {
@@ -78,7 +89,7 @@ func isPort(x int, z int, mapData *MapData) bool {
 }
 
 func getImagePosition(i int, j int) (float64, float64) {
-	radius := 10.0
+	radius := HexRadius
 	angle := math.Pi / 6
 
 	x := (radius * 1.5) + float64(j)*(2*radius*math.Cos(angle))
@@ -98,9 +109,129 @@ func removeAccents(str string) string {
 	return newStr
 }
 
-func drawMap(mapData *MapData, outputFilename string) {
-	radius := 10.0
+func getTileColor(tile *fileio.MapTile, mapData *MapData, x, z int) (r, g, b int) {
+	if tile.IsSea {
+		return 95, 149, 149
+	} else if isPort(x, z, mapData) {
+		return 75, 113, 224
+	} else {
+		switch tile.TileType {
+		case fileio.Grass:
+			return 105, 125, 54
+		case fileio.Sand:
+			return 200, 200, 164
+		case fileio.Farmland:
+			return 127, 121, 71
+		case fileio.Forest:
+			return 53, 72, 44
+		case fileio.Snow:
+			return 238, 249, 255
+		case fileio.Factory:
+			return 213, 95, 7
+		case fileio.Town:
+			return 1, 137, 26
+		case fileio.City:
+			return 87, 88, 80
+		default:
+			return 0, 0, 0
+		}
+	}
+}
 
+func drawMountain(dc *gg.Context, x, y float64) {
+	dc.DrawRegularPolygon(3, x, y, HexRadius, math.Pi)
+	dc.SetRGB255(89, 90, 86)
+	dc.Fill()
+	dc.DrawRegularPolygon(3, x, y+(HexRadius/2), HexRadius/2, math.Pi)
+	dc.SetRGB255(234, 244, 253)
+	dc.Fill()
+}
+
+func drawCityMarker(dc *gg.Context, x, y float64, tile *fileio.MapTile) {
+	if tile.HasFlag && tile.Party >= 0 {
+		// Draw capital city
+		dc.DrawCircle(x, y, HexRadius/2)
+		dc.SetRGB255(PartyColors[tile.Party][0], PartyColors[tile.Party][1], PartyColors[tile.Party][2])
+		dc.Fill()
+	} else {
+		dc.DrawRectangle(x-2.0, y-2.0, HexRadius/2, HexRadius/2)
+		dc.SetRGB255(255, 255, 255)
+		dc.Fill()
+	}
+}
+
+func drawTiles(dc *gg.Context, mapData *MapData) {
+	for i := 0; i < mapData.Depth; i++ {
+		for j := 0; j < mapData.Width; j++ {
+			x, y := getImagePosition(i, j)
+			dc.DrawRegularPolygon(6, x, y, HexRadius, math.Pi/2)
+
+			tile := mapData.MapTiles[j][i]
+			r, g, b := getTileColor(tile, mapData, j, i)
+			dc.SetRGB255(r, g, b)
+			dc.Fill()
+
+			if tile.IsMountain {
+				drawMountain(dc, x, y)
+			}
+
+			if tile.TileType == fileio.Factory ||
+				tile.TileType == fileio.City ||
+				tile.TileType == fileio.Town {
+				drawCityMarker(dc, x, y, tile)
+			}
+		}
+	}
+}
+
+func isValidNeighbor(x, z, width, depth int) bool {
+	return x >= 0 && z >= 0 && x < width && z < depth
+}
+
+func shouldDrawRoad(tile *fileio.MapTile) bool {
+	return tile.HasRoad || tile.TileType >= fileio.Factory
+}
+
+func drawRoads(dc *gg.Context, mapData *MapData) {
+	for i := 0; i < mapData.Depth; i++ {
+		for j := 0; j < mapData.Width; j++ {
+			if !mapData.MapTiles[j][i].HasRoad {
+				continue
+			}
+
+			x1, y1 := getImagePosition(i, j)
+			neighbors := getNeighbors(j, i)
+			for n := 0; n < len(neighbors); n++ {
+				newX := neighbors[n][0]
+				newZ := neighbors[n][1]
+				if isValidNeighbor(newX, newZ, mapData.Width, mapData.Depth) {
+					neighborTile := mapData.MapTiles[newX][newZ]
+					if shouldDrawRoad(neighborTile) {
+						x2, y2 := getImagePosition(newZ, newX)
+						dc.SetRGB255(78, 53, 36)
+						dc.DrawLine(x1, y1, x2, y2)
+						dc.Stroke()
+					}
+				}
+			}
+		}
+	}
+}
+
+func drawCityNames(dc *gg.Context, mapData *MapData) {
+	dc.InvertY()
+	for i := 0; i < mapData.Depth; i++ {
+		for j := 0; j < mapData.Width; j++ {
+			// Invert depth because the map is inverted
+			x, y := getImagePosition(mapData.Depth-i, j)
+			tile := mapData.MapTiles[j][i]
+			dc.SetRGB255(255, 255, 255)
+			dc.DrawString(removeAccents(tile.CityName), x-(5.0*float64(len(tile.CityName))/2.0), y-HexRadius*1.5)
+		}
+	}
+}
+
+func drawMap(mapData *MapData, outputFilename string) {
 	mapWidth := len(mapData.MapTiles)
 	mapDepth := len(mapData.MapTiles[0])
 
@@ -111,111 +242,35 @@ func drawMap(mapData *MapData, outputFilename string) {
 	// Need to invert image because the map format is inverted
 	dc.InvertY()
 
-	for i := 0; i < mapData.Depth; i++ {
-		for j := 0; j < mapData.Width; j++ {
-			x, y := getImagePosition(i, j)
-			dc.DrawRegularPolygon(6, x, y, radius, math.Pi/2)
-
-			tile := mapData.MapTiles[j][i]
-			if tile.IsSea {
-				dc.SetRGB255(95, 149, 149)
-			} else if isPort(j, i, mapData) {
-				dc.SetRGB255(75, 113, 224)
-			} else {
-				switch tile.TileType {
-				case fileio.Grass:
-					dc.SetRGB255(105, 125, 54)
-				case fileio.Sand:
-					dc.SetRGB255(200, 200, 164)
-				case fileio.Farmland:
-					dc.SetRGB255(127, 121, 71)
-				case fileio.Forest:
-					dc.SetRGB255(53, 72, 44)
-				case fileio.Snow:
-					dc.SetRGB255(238, 249, 255)
-				case fileio.Factory:
-					dc.SetRGB255(213, 95, 7)
-				case fileio.Town:
-					dc.SetRGB255(1, 137, 26)
-				case fileio.City:
-					dc.SetRGB255(87, 88, 80)
-				default:
-					dc.SetRGB255(0, 0, 0)
-				}
-			}
-
-			dc.Fill()
-
-			if tile.IsMountain {
-				dc.DrawRegularPolygon(3, x, y, radius, math.Pi)
-				dc.SetRGB255(89, 90, 86)
-				dc.Fill()
-				dc.DrawRegularPolygon(3, x, y+(radius/2), radius/2, math.Pi)
-				dc.SetRGB255(234, 244, 253)
-				dc.Fill()
-			}
-
-			if tile.TileType == fileio.Factory ||
-				tile.TileType == fileio.City ||
-				tile.TileType == fileio.Town {
-				if tile.HasFlag && tile.Party >= 0 {
-					// Draw capital city
-					dc.DrawCircle(x, y, radius/2)
-					dc.SetRGB255(PartyColors[tile.Party][0], PartyColors[tile.Party][1], PartyColors[tile.Party][2])
-					dc.Fill()
-				} else {
-					dc.DrawRectangle(x-2.0, y-2.0, radius/2, radius/2)
-					dc.SetRGB255(255, 255, 255)
-					dc.Fill()
-				}
-			}
-		}
-	}
-
-	// Draw roads between tiles
-	for i := 0; i < mapData.Depth; i++ {
-		for j := 0; j < mapData.Width; j++ {
-			x1, y1 := getImagePosition(i, j)
-
-			if !mapData.MapTiles[j][i].HasRoad {
-				continue
-			}
-
-			neighbors := getNeighbors(j, i)
-			for n := 0; n < len(neighbors); n++ {
-				newX := neighbors[n][0]
-				newZ := neighbors[n][1]
-				if newX >= 0 && newZ >= 0 && newX < mapData.Width && newZ < mapData.Depth {
-					if mapData.MapTiles[newX][newZ].HasRoad || mapData.MapTiles[newX][newZ].TileType >= fileio.Factory {
-						x2, y2 := getImagePosition(newZ, newX)
-						dc.SetRGB255(78, 53, 36)
-						dc.DrawLine(x1, y1, x2, y2)
-						dc.Stroke()
-					}
-				}
-			}
-		}
-	}
-
-	// Draw city names on top of hexes
-	dc.InvertY()
-	for i := 0; i < mapData.Depth; i++ {
-		for j := 0; j < mapData.Width; j++ {
-			// Invert depth because the map is inverted
-			x, y := getImagePosition(mapData.Depth-i, j)
-
-			tile := mapData.MapTiles[j][i]
-			dc.SetRGB255(255, 255, 255)
-			dc.DrawString(removeAccents(tile.CityName), x-(5.0*float64(len(tile.CityName))/2.0), y-radius*1.5)
-		}
-	}
+	drawTiles(dc, mapData)
+	drawRoads(dc, mapData)
+	drawCityNames(dc, mapData)
 
 	dc.SavePNG(outputFilename)
 	fmt.Println("Saved image to", outputFilename)
 }
 
+func printHelp() {
+	fmt.Println("HexEmpire3Map - Hex Empire 3 Map Tool")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("  hexmap -mode=<mode> -input=<input> [-output=<output>]")
+	fmt.Println()
+	fmt.Println("Modes:")
+	fmt.Println("  visualize  - Convert .he3 map file to PNG image")
+	fmt.Println("  decompress - Decompress .he3 file to binary data")
+	fmt.Println("  compress   - Compress binary data to .he3 format")
+	fmt.Println("  help       - Show this help message")
+	fmt.Println()
+	fmt.Println("Examples:")
+	fmt.Println("  hexmap -mode=visualize -input=maps/Europe.he3 -output=europe.png")
+	fmt.Println("  hexmap -mode=decompress -input=maps/Europe.he3 -output=europe.bin")
+	fmt.Println("  hexmap -mode=compress -input=europe.bin -output=europe_new.he3")
+	fmt.Println()
+}
+
 func main() {
-	availableModes := "[visualize, decompress, compress]"
+	availableModes := "[visualize, decompress, compress, help]"
 	modePtr := flag.String("mode", "", "Available modes: "+availableModes)
 	inputPtr := flag.String("input", "", "Input filename")
 	outputPtr := flag.String("output", "output.png", "Output filename")
@@ -224,6 +279,15 @@ func main() {
 	mode := *modePtr
 	inputFilename := *inputPtr
 	outputFilename := *outputPtr
+
+	if mode == "help" || mode == "" {
+		printHelp()
+		if mode == "" {
+			os.Exit(1)
+		}
+		return
+	}
+
 	fmt.Println("Mode: ", mode)
 	fmt.Println("Input filename: ", inputFilename)
 	fmt.Println("Output filename: ", outputFilename)
@@ -241,7 +305,7 @@ func main() {
 			log.Fatal("Failed to write to output file: ", err)
 		}
 	} else if mode == "compress" {
-		decompressedBytes, err := ioutil.ReadFile(inputFilename)
+		decompressedBytes, err := os.ReadFile(inputFilename)
 		if err != nil {
 			log.Fatal("Failed to read input file: ", err)
 		}
@@ -251,6 +315,8 @@ func main() {
 			log.Fatal("Failed to write to output file: ", err)
 		}
 	} else {
-		log.Fatal("Invalid mode. One of the following modes are supported " + availableModes)
+		fmt.Println("Invalid mode. One of the following modes are supported " + availableModes)
+		fmt.Println("Use -mode=help for usage information")
+		os.Exit(1)
 	}
 }
